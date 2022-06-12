2a1b729f8053af38f89d7f2cf97a7f532c9c521a
wouleuf <wouleuf@gmail.com>
2022-06-12 06:27:06 +0100

    add conjured test

---
+++ src/test/java/com/gildedrose/ConjuredTest.java
@@ -0,0 +1,18 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConjuredTest {
+
+    @Test
+    public void conjuredItemsDegradeInQualityTwiceAsFastAsNormalItems() throws Exception {
+        Item[] items = new Item[] { new Item("Conjured", 2, 36) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Conjured", app.items[0].name);
+        assertEquals(34, app.items[0].quality);
+        assertEquals(1, app.items[0].sellIn);
+    }
+}
